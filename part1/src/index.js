2f2b39cf32867196c5cb26d47419335f94a33f23
Francesco Fano <francesco.fano@xailabs.de>
2020-07-14 12:19:23 +0200

    part 2.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,49 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app.js";
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(
-    Array.apply(null, new Array(anecdotes.length)).map(
-      Number.prototype.valueOf,
-      0
-    )
-  );
-
-  const selectRandom = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  const vote = () => {
-    const copy = [...votes];
-    copy[selected] += 1;
-    setVotes(copy);
-  };
-
-  return (
-    <div>
-      <h1>Anectode of the day</h1>
-      <p>
-        {props.anecdotes[selected]} has {votes[selected]} votes
-      </p>
-      <button onClick={() => vote()}>vote</button>
-      <button onClick={() => selectRandom()}>next anecdote</button>
-      <h1>Anectode with most votes </h1>
-      <p>
-        {props.anecdotes[votes.indexOf(Math.max(...votes))]} has{" "}
-        {votes[votes.indexOf(Math.max(...votes))]} votes
-      </p>
-    </div>
-  );
-};
-
-const anecdotes = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
