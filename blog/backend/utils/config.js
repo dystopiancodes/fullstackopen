d14fd01178a2e09ad7b8f238eaeb2074fdcf0a88
Francesco Fano <francesco.fano@xailabs.de>
2020-07-22 11:28:26 +0200

    adding lint + start testing

--- blog/backend/utils/config.js
+++ blog/backend/utils/config.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const PORT = 3003;
-const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = 3003
+const MONGODB_URI = process.env.MONGODB_URI
 module.exports = {
   MONGODB_URI,
   PORT,
-};
+}
